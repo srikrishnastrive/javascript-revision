bfa4a4848d2f1d16ff969a440f8b834fa9438943
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-05-17 22:30:38 +0530

    added the coercisions

---
+++ 5_additoin_coercion.js
@@ -0,0 +1,12 @@
+
+let value = "5";
+let value1 = 9;
+
+console.log(value+value1);
+
+let obj = {};
+
+console.log(obj);
+
+console.log("18"+obj);
+console.log(18+obj);
---
+++ 8_abstract_equality.js
@@ -0,0 +1,9 @@
+console.log(null == undefined);
+
+console.log(2 == "2");
+//"2" --> coercoin --> string --> number --> 2 = 2 === 2 --> strict equality--> true
+
+
+//booleam
+
+console.log(1 == true);
